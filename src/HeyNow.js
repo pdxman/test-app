882953ad033045d1d6782e566228453d0f0368fd
Crawford Vining <crawfordvining@fm-cravin2-mac.home>
2021-07-31 13:05:13 -0700

    added updated function for buttons (words)

--- src/HeyNow.js
+++ src/HeyNow.js
@@ -4,10 +4,14 @@ import React, { useState } from 'react'
 export default function HeyNow(){
     const [words, setWords] = useState('words')
 
+    function updateWords() {
+        setWords('These are the NEW words!')
+    }
+
     return(
         <div>
              <h1>Hey Now!</h1>
-             <button onClick={() => alert('Hey Now!')}>This is the button to do stuff!</button>
+             <button onClick={updateWords}>This is the button to do stuff!</button>
              <p>{words}</p>
         </div>
     )
